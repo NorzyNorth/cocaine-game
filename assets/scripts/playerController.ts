ecd1d3931a32c7df75a74bfd50d3f5c042d7fb5a
Rais3322 <Rais3322@yandex.ru>
2024-01-24 15:53:19 +0300

    made-walk

--- assets/scripts/playerController.ts
+++ assets/scripts/playerController.ts
@@ -1,11 +1,6 @@
 import {
   _decorator,
   Component,
-  EventKeyboard,
-  macro,
-  Node,
-  systemEvent,
-  SystemEventType,
   Vec3,
   input as engineInput,
   Input as EngineInput,
@@ -22,13 +17,19 @@ enum _InputEventType {
 @ccclass("playerController")
 export class PlayerController extends Component {
   private _movementDirection: Vec3;
-  private _movementSpeed: number = 1;
-  private _gravity: number = 9.81;
-  private _jumpPower: number = 10;
   private _gameInput: GameInput;
   private _velocity: Vec3;
   private _characterController: CharacterController;
-  private _
+  private _velocityY: number = 0.0;
+
+  @property
+  private _movementSpeed: number = 7;
+
+  @property
+  private _gravity: number = 9.81;
+
+  @property
+  private _jumpPower: number = 10;
 
   protected start(): void {
     this._gameInput = new GameInput();
@@ -37,7 +38,10 @@ export class PlayerController extends Component {
   }
 
   protected update(dt: number): void {
-    this.move();
+    this.applyGravity(dt);
+    // console.log(this._velocityY);
+    this.move(dt);
+    console.log(this._velocity);
   }
 
   private applyGameInput(): void {
@@ -49,13 +53,20 @@ export class PlayerController extends Component {
     );
   }
 
-  private move() {
+  private move(dt) {
+    let movementDistance = this._movementSpeed * dt;
     this._movementDirection = this._gameInput.getInputDirection();
     this._velocity = new Vec3(
-      this._movementDirection.x * this._movementSpeed,
-      this._movementDirection.y * this._movementSpeed,
-      this._movementDirection.z * this._movementSpeed
+      this._movementDirection.x * movementDistance,
+      this._velocityY,
+      this._movementDirection.z * movementDistance
     );
     this._characterController.move(this._velocity);
+  }
+
+  private applyGravity(dt: number) {
+    if (this._velocityY >= -200) {
+      this._velocityY += -this._gravity * dt;
+    }
   }
 }
