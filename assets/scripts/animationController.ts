8c9d8877585fa9b28180a3934178ccd309c41f3b
Rais3322 <Rais3322@yandex.ru>
2024-01-29 16:50:10 +0300

    trying-animations

--- assets/scripts/animationController.ts
+++ assets/scripts/animationController.ts
@@ -1,18 +1,39 @@
-import { _decorator, Component, Node } from "cc";
+import { _decorator, animation, Component } from "cc";
+import { GameInput } from "./gameInput";
 const { ccclass, property } = _decorator;
 
 @ccclass("animationController")
-export class AnimationController extends Component {
+export class AnimeController extends Component {
   @property
-  public _isWalking: boolean = true;
+  public _animationController: animation.AnimationController;
+  public _gameInput: GameInput;
+  public _variables;
 
-  start() {}
+  start() {
+    this._gameInput = new GameInput();
+    this._animationController = this.node.getComponent(
+      animation.AnimationController
+    );
+    this._variables = this._animationController.getVariables();
+  }
 
-  update(deltaTime: number) {}
+  update(deltaTime: number) {
+    this.useIdle();
+    this.useWalk();
+  }
+
+  useIdle() {
+    if (!this._gameInput.getMovementInput()) {
+      this._animationController.setValue("walking", false);
+    }
+  }
+
+  useWalk() {
+    console.log(this._gameInput.getMovementInput())
+    if (this._gameInput.getMovementInput()) {
+      this._animationController.setValue("walking", true);
+    }
+  }
 
   useRun() {}
-
-  useIdle() {}
-
-  useWalk() {}
 }
