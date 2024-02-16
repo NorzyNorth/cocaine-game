c8d84e6a50271248536076aa958e8d56c9a9e1c1
norzyNorth <musnorzy@gmail.com>
2024-02-16 12:41:17 +0300

    add unworked rot

--- assets/scripts/multiplayer/renderMultiplayer.ts
+++ assets/scripts/multiplayer/renderMultiplayer.ts
@@ -1,77 +1,84 @@
-import { _decorator, Component, Node, Prefab, instantiate, assetManager, resources, director } from 'cc';
+import {
+  _decorator,
+  Component,
+  Node,
+  Prefab,
+  instantiate,
+  assetManager,
+  resources,
+  director,
+} from "cc";
 const { ccclass, property } = _decorator;
 
-@ccclass('renderMultiplayer')
+@ccclass("renderMultiplayer")
 export class renderMultiplayer extends Component {
-    // @property({ type: Prefab })
-    // modelPrefab: Prefab = null;
-    // start() {
-    //     this.node = this.node
-    // }
-    // executeCode() {
-    //     if (!this.modelPrefab) {
-    //         this.loadPrefabFromURL('https://s3.timeweb.com/466b5258-a96bb5a0-7ffe-42b3-93e5-dd669d0f8f00/Player-blue.prefab')
-    //             .then((prefab: Prefab) => {
-    //                 this.modelPrefab = prefab;
-    //                 console.log(this.modelPrefab);
-                    
-    //                 this.createModel();
-    //             })
-    //             .catch((error: Error) => {
-    //                 console.error('Failed to load prefab:', error);
-    //             });
-    //     } else {
-    //         this.createModel();
-    //     }
-    // }
+  // @property({ type: Prefab })
+  // modelPrefab: Prefab = null;
+  // start() {
+  //     this.node = this.node
+  // }
+  // executeCode() {
+  //     if (!this.modelPrefab) {
+  //         this.loadPrefabFromURL('https://s3.timeweb.com/466b5258-a96bb5a0-7ffe-42b3-93e5-dd669d0f8f00/Player-blue.prefab')
+  //             .then((prefab: Prefab) => {
+  //                 this.modelPrefab = prefab;
+  //                 console.log(this.modelPrefab);
 
-    // loadPrefabFromURL(url: string): Promise<Prefab> {
-    //     return new Promise((resolve, reject) => {
-    //         assetManager.loadAny({ url }, (err, prefab) => {
-    //             if (err) {
-    //                 reject(err);
-    //             } else {
-    //                 resolve(prefab as Prefab);
-    //             }
-    //         });
-    //     });
-    // }
+  //                 this.createModel();
+  //             })
+  //             .catch((error: Error) => {
+  //                 console.error('Failed to load prefab:', error);
+  //             });
+  //     } else {
+  //         this.createModel();
+  //     }
+  // }
 
-    // createModel() {
-    //     const emptyNode = new Node('kekemode');
-    //     this.node.getParent().addChild(emptyNode);
+  // loadPrefabFromURL(url: string): Promise<Prefab> {
+  //     return new Promise((resolve, reject) => {
+  //         assetManager.loadAny({ url }, (err, prefab) => {
+  //             if (err) {
+  //                 reject(err);
+  //             } else {
+  //                 resolve(prefab as Prefab);
+  //             }
+  //         });
+  //     });
+  // }
 
-    //     if (this.modelPrefab) {
-    //         const modelNode = instantiate(this.modelPrefab);
-    //         emptyNode.addChild(modelNode);
-    //     } else {
-    //         console.error('Model prefab is not assigned.');
-    //     }
-    // // }
-    // createObject()  {
-    //     let newObject : Node
-    //     resources.load("player-blue/player-blue", Prefab, (_err, prefab) => {
-    //       newObject = instantiate(prefab);
-    //       director.getScene().addChild(newObject);
-    //       return newObject
-    //     })
-    //   }
-    createObject(): Promise<Node> {
-        return new Promise((resolve, reject) => {
-          resources.load("player-blue/player-blue", Prefab, (_err, prefab) => {
-            if (prefab) {
-              const newObject = instantiate(prefab);
-              director.getScene().addChild(newObject);
-              resolve(newObject);
-            } else {
-              reject("Failed to load prefab");
-            }
-          });
-        });
-      }
-      destroyObject(node : Node) {
-        node.destroy()
-      }
+  // createModel() {
+  //     const emptyNode = new Node('kekemode');
+  //     this.node.getParent().addChild(emptyNode);
+
+  //     if (this.modelPrefab) {
+  //         const modelNode = instantiate(this.modelPrefab);
+  //         emptyNode.addChild(modelNode);
+  //     } else {
+  //         console.error('Model prefab is not assigned.');
+  //     }
+  // // }
+  // createObject()  {
+  //     let newObject : Node
+  //     resources.load("player-blue/player-blue", Prefab, (_err, prefab) => {
+  //       newObject = instantiate(prefab);
+  //       director.getScene().addChild(newObject);
+  //       return newObject
+  //     })
+  //   }
+  createObject(): Promise<Node> {
+    return new Promise((resolve, reject) => {
+      resources.load("player-blue/player-blue", Prefab, (_err, prefab) => {
+        if (prefab) {
+          const newObject = instantiate(prefab);
+          director.getScene().addChild(newObject);
+          resolve(newObject);
+        } else {
+          reject("Failed to load prefab");
+        }
+      });
+    });
+  }
+  destroyObject(node: Node) {
+    if (node) node.destroy();
+  }
 }
-
-
